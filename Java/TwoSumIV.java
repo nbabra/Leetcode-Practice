72c8d1f6e8fe09f9e9899b608af7f97ddd2f7d08
Navneeth Babra <nbabra@umd.edu>
2018-11-07 15:34:36 -0500

    Create TwoSumIV.java

---
+++ Java/TwoSumIV.java
@@ -0,0 +1,56 @@
+/*
+
+ Given a Binary Search Tree and a target number, return true if there exist two elements in the BST such that their sum is equal to the given target.
+
+ Example 1:
+
+ Input: 
+     5
+    / \
+   3   6
+  / \   \
+ 2   4   7
+
+ Target = 9
+
+ Output: True
+ Example 2:
+
+ Input: 
+     5
+    / \
+   3   6
+  / \   \
+ 2   4   7
+
+ Target = 28
+
+ Output: False
+
+*/
+
+ /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean findTarget(TreeNode root, int target) {
+        Set<Integer> set = new HashSet<Integer>();
+        return this.findTarget(root, target, set);
+    }
+    private boolean findTarget(TreeNode root, int target, Set<Integer> set) {
+        if (root == null) {
+            return false;
+        }
+        if (set.contains(target-root.val)) {
+            return true;
+        }
+        set.add(root.val);
+        return findTarget(root.left, target, set) || findTarget(root.right, target, set);
+    }
+}
